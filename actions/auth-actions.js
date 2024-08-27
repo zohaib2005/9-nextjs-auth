e6403244b2b8d784638e6ec2f2ac54203d5fd8bb
zohaib <zohaibshafiq2005@gmail.com>
2024-08-27 15:30:08 +0500

    session cookies with lucia

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
+import { createAuthSession } from "@/lib/auth";
 
 export async function signup(prevState, formData) {
   const email = formData.get("email");
@@ -24,7 +25,9 @@ export async function signup(prevState, formData) {
   }
   const hashedPassword = hashUserPassword(password);
   try {
-    createUser(email, hashedPassword);
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/training");
   } catch (error) {
     if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
       return {
@@ -36,5 +39,4 @@ export async function signup(prevState, formData) {
     }
     throw error;
   }
-  redirect("/training");
 }
